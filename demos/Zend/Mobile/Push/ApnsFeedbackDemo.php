4c2f740dc4d1e1426a4bf6ee702d4702beb49a6a
Mike Willbanks <mike@digitalstruct.com>
2011-12-20 23:52:03 -0600

    updated apns feedback demo for change in the return

--- demos/Zend/Mobile/Push/ApnsFeedbackDemo.php
+++ demos/Zend/Mobile/Push/ApnsFeedbackDemo.php
@@ -5,6 +5,6 @@ $apns = new Zend_Mobile_Push_Apns();
 $apns->setCertificate(dirname(__FILE__) . '/mycert.pem'); // REPLACE WITH YOUR CERT
 
 $tokens = $apns->feedback();
-foreach ($tokens as $t) {
-    echo $t['token'] . ': ' . $t['time'] . PHP_EOL;
+foreach ($tokens as $token => $time) {
+    echo $token . ': ' . $time . PHP_EOL;
 }
