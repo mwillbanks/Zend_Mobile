000e657f3a97545f0f055afbd6a97b21b7c9a88d
Mike Willbanks <mike@digitalstruct.com>
2011-12-27 08:12:13 -0600

    added shutup operator to fwrite due to warnings

--- library/Zend/Mobile/Push/Apns.php
+++ library/Zend/Mobile/Push/Apns.php
@@ -231,7 +231,7 @@ class Zend_Mobile_Push_Apns extends Zend_Mobile_Push_Abstract
         $payload = pack('CNNnH*', 1, $id, $expire, 32, $message->getToken())
             . pack('n', strlen($payload))
             . $payload;
-        $ret = fwrite($this->_socket, $payload);
+        $ret = @fwrite($this->_socket, $payload);
         if ($ret === false) {
             require_once 'Zend/Mobile/Push/Exception/ServerUnavailable.php';
             throw new Zend_Mobile_Push_Exception_ServerUnavailable('Unable to send message');
