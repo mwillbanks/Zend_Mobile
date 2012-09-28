51eead2f41a203aa0c345069c7606127ad50b5d3
Mario Giustiniani <mario.giustiniani@mashfrog.com>
2012-09-28 16:40:31 +0200

    add blackberry demoo

---
+++ demos/Zend/Mobile/Push/PapServer.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'Zend/Mobile/Push/Pap.php';
+require_once 'Zend/Mobile/Push/Message/Pap.php';
+
+$pap = new \Zend_Mobile_Push_Pap('xxxxxxxxx', 'xxxxxxxxx');
+$pap->setEnvironment('prod');
+if ($env == 'prod')
+	$this->pap->setContentProviderId('000');
+
+}
+
+	$message = new \Zend_Mobile_Push_Message_Pap('Zend Mobile Push Example', null, '+5 seconds');
+	
+	$message->addTo('push_all');
+	
+
+	$response = $this->pap->send($message);
+	if ($response->isError()) {
+		echo 'Zend Mobile Push::Descrizione errore: '
+		. $response->getErrorString();
+
+	} else
+		echo 'Zend Mobile Push::Descrizione risposta: '
+		. $response->getResponseDesc();
+
+}
+
+
